63633051794c380dfe685ac7994064b0b30e8f79
korolanatolii <akorol@flow-tech.ai>
2021-07-24 20:17:02 +0300

    gen mask with erosion for weakly training

--- gen_masks.py
+++ gen_masks.py
@@ -1,6 +1,7 @@
 import os
-import numpy as np
 from tqdm import tqdm
+import numpy as np
+from scipy import ndimage
 
 import torch
 from torch.utils.data import DataLoader
@@ -9,14 +10,32 @@ from data.dataset import ISPRS_Dataset
 from models.Unet import UNET
 
 configs = {
-    'path_to_save': 'data/weakly_masks',
+    'path_to_weakly_masks': 'data/preprocessed/weakly_masks',
+    'path_to_weak_mask_erosion': 'data/preprocessed/weakly_masks_erosion',
     'model_path': 'checkpoints/baseline_Unet.pth'
 }
 
 
-def get_masks(model, dataloader, device, path_to_save):
-    if not os.path.exists(path_to_save):
-        os.makedirs(path_to_save)
+def makedirs(dirs: str):
+    if not os.path.exists(dirs):
+        os.makedirs(dirs)
+
+
+def erosion(img):
+    img = img.copy()
+    for i in range(4):
+        img[i] = ndimage.binary_dilation(img[i], iterations=10).astype(int)
+
+    orig_img = img.copy()
+    for i in range(4):
+        img[i] = ndimage.binary_erosion(img[i], iterations=20).astype(int)
+
+    return img - (orig_img - img)
+
+
+def get_masks(model, dataloader, device, path_to_weakly_masks, path_to_weak_mask_erosion):
+    makedirs(path_to_weakly_masks)
+    makedirs(path_to_weak_mask_erosion)
 
     model.eval()
 
@@ -38,7 +57,11 @@ def get_masks(model, dataloader, device, path_to_save):
         pred_masks[img_label == 0] = 0
 
         for i in range(0, len(imgs)):
-            np.save(f'{path_to_save}/{file_names[i]}', pred_masks[i])
+
+            np.save(f'{path_to_weakly_masks}/{file_names[i]}', pred_masks[i])
+
+            pred_masks[i] = erosion(pred_masks[i])
+            np.save(f'{path_to_weak_mask_erosion}/{file_names[i]}', pred_masks[i])
 
 
 if __name__ == '__main__':
@@ -51,4 +74,10 @@ if __name__ == '__main__':
     dataset = ISPRS_Dataset('data/preprocessed', 'data/preprocessed/metadata.csv', 'weak_train')
     dataloader = DataLoader(dataset, shuffle=False, batch_size=3)
 
-    get_masks(model, dataloader, device, configs['path_to_save'])
+    get_masks(
+        model=model,
+        dataloader=dataloader,
+        device=device,
+        path_to_weakly_masks=configs['path_to_weakly_masks'],
+        path_to_weak_mask_erosion=configs['path_to_weak_mask_erosion']
+    )
